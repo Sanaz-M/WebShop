504eb22cc79c172a0c00db8791629a20c7975fdc
Sanaz M <sanazmohammaddi@gmail.com>
2022-02-13 18:50:56 +0000

    updates in carousel

--- vazi-vazi/src/components/homepage/DealOffer.jsx
+++ vazi-vazi/src/components/homepage/DealOffer.jsx
@@ -7,7 +7,7 @@ const DealOffer = () => {
     return (
         <Row className='justify-content-center'>
             <Col sm={6} md={5} id="deal-description">
-                <Col>
+                <Col id="deal-text">
                 <h3>DEAL OF THE WEAK</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                 <p>€80</p>
@@ -18,7 +18,7 @@ const DealOffer = () => {
             </Col>
             <Col sm={6} md={5} id="deal-photo">
                 <div>
-                    <img src={OfferPic} alt="..." width="450" height="450" />
+                    <img src={OfferPic} alt="..." height="450" />
                 </div>
             </Col>
         </Row>
