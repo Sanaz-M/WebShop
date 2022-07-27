631f9f6ae2abd6be15eae1fa894dd69929e76d88
Sanaz M <sanazmohammaddi@gmail.com>
2022-07-27 12:44:06 +0200

    update the cart reducer

--- vazi-vazi/src/redux/reducers/cartReducer.js
+++ vazi-vazi/src/redux/reducers/cartReducer.js
@@ -1,13 +1,23 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, REMOVE_FROM_QUANTITY, ADD_TO_QUANTITY } from '../action';
+import { ADD_TO_CART, REMOVE_FROM_CART, ADD_TO_QUANTITY } from '../action';
 import { initialState } from '../store';
 
 const cartReducer = (state = initialState.cart, action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      return {
-        ...state,
-        content: [...state.content, action.payload],
-      }
+      const addedItem = state.content.find(item => item.id === action.payload.id)
+      if(addedItem >= 0){
+        state.content[addedItem].qty +=1
+        return {
+            ...state,
+            content:[...state.content]
+        }
+    } else {
+        const temp = {...action.payload,qty:1}
+         return {
+            ...state,
+            content: [...state.content, temp]
+        }
+    }
 
     case REMOVE_FROM_CART:
       return {
@@ -15,17 +25,11 @@ const cartReducer = (state = initialState.cart, action) => {
         content: state.content.filter((product) => product !== action.payload),
       }
 
-    case ADD_TO_QUANTITY:
-      return {
-        ...state,
-        quantity: action.payload,
-      }
-
-    case REMOVE_FROM_QUANTITY:
-      return {
-        ...state,
-        quantity: action.payload,
-      }
+    // case ADD_TO_QUANTITY:
+    //   return {
+    //     ...state,
+    //     content: state.content.map(item => item.id === action.payload.id ? {...item, qty: action.payload.qty} : item)
+    //   }
 
     default:
       return state
